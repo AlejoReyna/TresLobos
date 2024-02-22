0aaceb8f690e303f9a862b4344455574486a7fa7
Alexis Reyna <99472437+AlejoReyna@users.noreply.github.com>
2024-02-22 02:47:35 -0600

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav className="navbar bg-dark">
+            <div className="container-fluid d-flex">
+                <div class="navbar-brand">
+                    Tres Lobos
+                </div>
+            </div>
+
+        </nav>
+    );
+};
+
+export default Navbar;
